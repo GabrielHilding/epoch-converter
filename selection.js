f0243e56c63403ac5e38ac28ce122361efd8cd6b
Gabriel Hildingsson <gabbe.hildingsson@gmail.com>
2025-01-07 14:26:27 +0100

    added microseconds

--- selection.js
+++ selection.js
@@ -22,9 +22,12 @@ $(document).ready(function () {
 function processSelection(e) {
   let text = getSelectedText();
 
-  if ($.isNumeric(text) && [10, 13].includes(text.length)) {
+  if ($.isNumeric(text) && [10, 13,16].includes(text.length)) {
     if (text.length == 13) {  // Handle millisecond timestamps
       text = text / 1000;
+    }
+    if (text.length == 16) {  // Handle microsecond timestamps
+      text = text / 1000000;
     }
     var date = timestampToDate(text);
     showBubble(e, getLocalString(date), getUTCString(date));
